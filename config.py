9d9b11862712c46ece716eb98cb4fc0ad0fb83f3
jlewittes <jlewittes@kpmg.com>
2020-08-18 11:51:34 +0300

    remove secret key

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ class Config(object):
     DEBUG = False
     TESTING = False
     CSRF_ENABLED = True
-    SECRET_KEY = 'qwerty123456'
+    # SECRET_KEY = 'qwerty123456'
     SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 
